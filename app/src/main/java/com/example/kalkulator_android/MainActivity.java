49b351293b65ee0fc678d5a156f3e9260574c6fa
Kamil Wieczorek <75673575+Glutusek@users.noreply.github.com>
2021-11-15 10:19:30 +0100

    Dalsze próby obliczania (MainActivity.java)

--- app/src/main/java/com/example/kalkulator_android/MainActivity.java
+++ app/src/main/java/com/example/kalkulator_android/MainActivity.java
@@ -1,4 +1,11 @@
+/**
+ *  @author Kamil Wieczorek
+ *  @version %I%, %G%
+ *
+ *  Calculator
+ */
 package com.example.kalkulator_android;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,7 +24,33 @@ public class MainActivity extends AppCompatActivity {
 
     TextView result;
     boolean activeOperation = false;
-    char [] operators = "+-*/^.".toCharArray();
+    char [] operators = "+-*/.".toCharArray();
+    boolean error = false;
+
+    String [] buttonsToTurnOff = {
+            "button_percent",
+            "button_reverse",
+            "button_sqrt",
+            "button_sqr",
+            "button_division",
+            "button_multiplication",
+            "button_subtraction",
+            "button_addition",
+            "button_opposite",
+            "button_dot",
+            "button_equation",
+            "button_BKSP",
+            "button_1",
+            "button_2",
+            "button_3",
+            "button_4",
+            "button_5",
+            "button_6",
+            "button_7",
+            "button_8",
+            "button_9",
+            "button_0"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +61,26 @@ public class MainActivity extends AppCompatActivity {
         result.setText("0");
     }
 
+    /**
+     * Clearing the result
+     * @param view Clicked button
+     */
     public void clear(View view) {
         result.setText("0");
         activeOperation = false;
+        error = false;
+
+        for(String ID : buttonsToTurnOff) {
+            int tileID = getResources().getIdentifier(ID, "id", getPackageName());
+            Button tempButton = findViewById(tileID);
+            tempButton.setEnabled(true);
+        }
     }
 
+    /**
+     * Clearing the last character of the result
+     * @param view Clicked button
+     */
     public void backspace(View view) {
         String actualResult = result.getText().toString();
 
@@ -44,7 +92,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Writing a number into the result
+     * @param view Clicked button
+     */
     public void number(View view) {
+        if(error) {
+            for(String ID : buttonsToTurnOff) {
+                int tileID = getResources().getIdentifier(ID, "id", getPackageName());
+                Button tempButton = findViewById(tileID);
+                tempButton.setEnabled(true);
+            }
+            error = true;
+        }
+
         Button tempButton = findViewById(view.getId());
         String actualResult = result.getText().toString();
         String number = tempButton.getText().toString();
@@ -56,30 +117,50 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Writing "+" into the result
+     * @param view Clicked button
+     */
     public void addition(View view) {
         if(!result.getText().toString().equals("0")) {
             writeOperator("+");
         }
     }
 
+    /**
+     * Writing "-" into the result
+     * @param view Clicked button
+     */
     public void subtraction(View view) {
         if(!result.getText().toString().equals("0")) {
             writeOperator("-");
         }
     }
 
+    /**
+     * Writing "*" into the result
+     * @param view Clicked button
+     */
     public void multiplication(View view) {
         if(!result.getText().toString().equals("0")) {
             writeOperator("*");
         }
     }
 
+    /**
+     * Writing "/" into the result
+     * @param view Clicked button
+     */
     public void division(View view) {
         if(!result.getText().toString().equals("0")) {
             writeOperator("/");
         }
     }
 
+    /**
+     * Writing "%" into the result
+     * @param view Clicked button
+     */
     public void percent(View view) {
         String actualResult = result.getText().toString();
 
@@ -88,30 +169,103 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Reversing given result and writing into the result
+     * @param view Clicked button
+     * @exception Exception If could not count properly
+     */
     public void reversion(View view) {
+        String actualResult = result.getText().toString();
+        try {
+            double num = Double.parseDouble(actualResult);
+            result.setText(Double.toString(1.0 / num));
+        } catch (Exception e) {
+            result.setText("Błędne dane");
+            error = true;
 
-    }
-
-    public void sqrt(View view) {
-
-    }
-
-    public void sqr(View view) {
-        if(!result.getText().toString().equals("0")) {
-            writeOperator("^");
+            for(String ID : buttonsToTurnOff) {
+                int tileID = getResources().getIdentifier(ID, "id", getPackageName());
+                Button tempButton = findViewById(tileID);
+                tempButton.setEnabled(false);
+            }
         }
     }
 
-    public void opposition(View view) {
+    /**
+     * Writing square root of given number into the result
+     * @param view Clicked button
+     * @exception Exception If could not count properly
+     */
+    public void sqrt(View view) {
+        String actualResult = result.getText().toString();
+        try {
+            double num = Double.parseDouble(actualResult);
 
+            if(num < 0)
+                throw new Exception();
+
+            result.setText(Double.toString(Math.sqrt(num)));
+        } catch (Exception e) {
+            result.setText("Błędne dane");
+            error = true;
+
+            for(String ID : buttonsToTurnOff) {
+                int tileID = getResources().getIdentifier(ID, "id", getPackageName());
+                Button tempButton = findViewById(tileID);
+                tempButton.setEnabled(false);
+            }
+        }
     }
 
+    /**
+     * Writing square of given number into the result
+     * @param view Clicked button
+     * @exception Exception If could not count properly
+     */
+    public void sqr(View view) {
+        String actualResult = result.getText().toString();
+        try {
+            double num = Double.parseDouble(actualResult);
+            result.setText(Double.toString(Math.pow(num, 2)));
+        } catch (Exception e) {
+            result.setText("Błędne dane");
+            error = true;
+
+            for(String ID : buttonsToTurnOff) {
+                int tileID = getResources().getIdentifier(ID, "id", getPackageName());
+                Button tempButton = findViewById(tileID);
+                tempButton.setEnabled(false);
+            }
+        }
+    }
+
+    /**
+     * Changing positive number into negative number (or vice versa)
+     * @param view Clicked button
+     */
+    public void opposition(View view) {
+        String actualResult = result.getText().toString();
+
+        if(actualResult.indexOf("-") == 0)
+            result.setText(actualResult.substring(1));
+        else
+            result.setText("-" + actualResult);
+    }
+
+    /**
+     * Writing "." into the result
+     * @param view Clicked button
+     */
     public void floating_point(View view) {
         if(!result.getText().toString().equals("0")) {
             writeOperator(".");
         }
     }
 
+    /**
+     * Counting given sequence and writing into the result
+     * @param view Clicked button
+     */
     public void equation(View view) {
         String actualResult = result.getText().toString();
 
@@ -123,13 +277,22 @@ public class MainActivity extends AppCompatActivity {
             put('-', 1);
             put('*', 2);
             put('/', 2);
-            put('^', 3);
         }};
 
         int idx = -1;
+        boolean negativeNum = false;
+
+        if(actualResult.substring(0,1).equals("-"))
+            negativeNum = true;
 
         for(char ch : actualResult.toCharArray()) {
             idx++;
+
+            if(negativeNum) {
+                negativeNum = false;
+                continue;
+            }
+
             for(char op : operators) {
                 if(ch == op) {
                     operatorIndexes.add(idx);
@@ -147,9 +310,6 @@ public class MainActivity extends AppCompatActivity {
                         case '/':
                             actualOperators.add('/');
                             break;
-                        case '^':
-                            actualOperators.add('^');
-                            break;
                     }
                 }
             }
@@ -161,8 +321,8 @@ public class MainActivity extends AppCompatActivity {
 
         for(int i = 0; i < operatorIndexes.size()-1; i++) {
             numbers.add(actualResult.substring(
-                            operatorIndexes.get(i)+1,
-                            operatorIndexes.get(i+1)
+                    operatorIndexes.get(i)+1,
+                    operatorIndexes.get(i+1)
             ));
         }
 
@@ -170,8 +330,10 @@ public class MainActivity extends AppCompatActivity {
                 operatorIndexes.get(operatorIndexes.size()-1)+1
         ));
 
-        if(numbers.size() >= 1) {
-            for(int priority = 3; priority >= 1; priority--) {
+        Toast.makeText(getApplicationContext(), numbers.toString(), Toast.LENGTH_SHORT).show();
+
+        if(numbers.size() >= 2) {
+            for(int priority = 2; priority >= 1; priority--) {
 
                 int actualOperatorNum = 0;
 
@@ -180,14 +342,28 @@ public class MainActivity extends AppCompatActivity {
                         String tempNum = "";
 
                         switch (priority) {
-                            case 3:
-                                tempNum = power(numbers.get(actualOperatorNum), numbers.get(actualOperatorNum+1));
-                                break;
                             case 2: {
                                 if(op == '*') {
                                     tempNum = multiply(numbers.get(actualOperatorNum), numbers.get(actualOperatorNum+1));
                                 } else {
-                                    tempNum = divide(numbers.get(actualOperatorNum), numbers.get(actualOperatorNum+1));
+                                    double a = Double.parseDouble(numbers.get(actualOperatorNum));
+                                    double b = Double.parseDouble(numbers.get(actualOperatorNum+1));
+
+                                    try {
+                                        if(b == 0)
+                                            throw new Exception();
+
+                                        tempNum = Double.toString(a / b);
+                                    } catch (Exception e) {
+                                        result.setText("Błędne dane");
+                                        error = true;
+
+                                        for(String ID : buttonsToTurnOff) {
+                                            int tileID = getResources().getIdentifier(ID, "id", getPackageName());
+                                            Button tempButton = findViewById(tileID);
+                                            tempButton.setEnabled(false);
+                                        }
+                                    }
                                 }
                                 break;
                             }
@@ -202,14 +378,19 @@ public class MainActivity extends AppCompatActivity {
                         }
                         numbers.set(actualOperatorNum, tempNum);
                         numbers.remove(actualOperatorNum + 1);
-                        Toast.makeText(getApplicationContext(), numbers.toString(), Toast.LENGTH_SHORT).show();
                     }
                     actualOperatorNum++;
                 }
             }
+            if(!error)
+                result.setText(numbers.get(0));
         }
     }
 
+    /**
+     * Writing an operator into the result
+     * @param op Given operator to write
+     */
     private void writeOperator(String op) {
         String actualResult = result.getText().toString();
 
@@ -223,6 +404,12 @@ public class MainActivity extends AppCompatActivity {
         result.setText(actualResult + op);
     }
 
+    /**
+     * Adding both numbers
+     * @param left First number
+     * @param right Second number
+     * @return Sum of both numbers
+     */
     private String add(String left, String right) {
         double a = Double.parseDouble(left);
         double b = Double.parseDouble(right);
@@ -233,6 +420,12 @@ public class MainActivity extends AppCompatActivity {
         return tempStr;
     }
 
+    /**
+     * Subtracting both numbers
+     * @param left First number
+     * @param right Second number
+     * @return Subtraction of both numbers
+     */
     private String subtract(String left, String right) {
         double a = Double.parseDouble(left);
         double b = Double.parseDouble(right);
@@ -243,32 +436,17 @@ public class MainActivity extends AppCompatActivity {
         return tempStr;
     }
 
+    /**
+     * Multiplacting both numbers
+     * @param left First number
+     * @param right Second number
+     * @return Multiplication of both numbers
+     */
     private String multiply(String left, String right) {
         double a = Double.parseDouble(left);
         double b = Double.parseDouble(right);
 
         String tempStr = Double.toString(a * b);
-        tempStr = tempStr.substring(0, tempStr.indexOf('.'));
-
-        return tempStr;
-    }
-
-    private String divide(String left, String right) {
-        double a = Double.parseDouble(left);
-        double b = Double.parseDouble(right);
-
-        if(b != 0) {
-            return Double.toString(a / b);
-        }
-        else
-            return "";
-    }
-
-    private String power(String left, String right) {
-        double a = Double.parseDouble(left);
-        double b = Double.parseDouble(right);
-
-        String tempStr = Double.toString(Math.pow(a, b));
         tempStr = tempStr.substring(0, tempStr.indexOf('.'));
 
         return tempStr;
